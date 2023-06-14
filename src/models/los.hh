94e6d2180b6f7a6c3e5cee3aff9b64297f3d5640
Andrew Thompson <andrew@hijacked.us>
2023-06-14 14:40:56 -0700

    Try to use less stack

--- src/models/los.hh
+++ src/models/los.hh
@@ -5,13 +5,13 @@
 
 #include "../common.hh"
 
-void PlotLOSPath(struct output *out, struct site source, struct site destination, char mask_value, const struct LR LR);
+void PlotLOSPath(struct output *out, struct site source, struct site destination, char mask_value, const struct LR *LR);
 void PlotPropPath(struct output *out, struct site source, struct site destination, unsigned char mask_value, FILE *fd,
-                  int propmodel, int knifeedge, int pmenv, const struct LR LR);
+                  int propmodel, int knifeedge, int pmenv, const struct LR *LR);
 void PlotLOSMap(struct output *out, struct site source, double altitude, char *plo_filename, bool use_threads,
-                const struct LR LR);
+                const struct LR *LR);
 void PlotPropagation(struct output *out, struct site source, double altitude, char *plo_filename, int propmodel, int knifeedge,
-                     int haf, int pmenv, bool use_threads, const struct LR LR);
-void PlotPath(struct output *out, struct site source, struct site destination, char mask_value, const struct LR LR);
+                     int haf, int pmenv, bool use_threads, const struct LR *LR);
+void PlotPath(struct output *out, struct site source, struct site destination, char mask_value, const struct LR *LR);
 
 #endif /* _LOS_HH_ */
