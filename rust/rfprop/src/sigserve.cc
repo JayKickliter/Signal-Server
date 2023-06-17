e932b3a4a5ced637e6108a22d1994684f3b94f24
Jay Kickliter <jay@kickliter.com>
2023-06-17 11:04:28 -0700

    Remove loops

--- rust/rfprop/src/sigserve.cc
+++ rust/rfprop/src/sigserve.cc
@@ -9,13 +9,10 @@ extern int handle_args(int argc, char *argv[], output *ret_out);
 namespace sigserve_wrapper {
 
 int init(const char *sdf_path, bool debug) { return ::init(sdf_path, debug); };
+
 Report handle_args(int argc, char *argv[])
 {
     Report report;
-    report.dbm = 0.0;
-    report.loss = 0.0;
-    report.field_strength = 0.0;
-
     output out;
 
     report.retcode = ::handle_args(argc, argv, &out);
@@ -27,38 +24,29 @@ Report handle_args(int argc, char *argv[])
     report.loss = out.loss;
     report.field_strength = out.field_strength;
 
-    for (auto &a : out.cluttervec) {
-        report.cluttervec.push_back(a);
-    }
+    report.cluttervec.reserve(out.cluttervec.size());
+    std::copy(out.cluttervec.begin(), out.cluttervec.end(), std::back_inserter(report.cluttervec));
 
-    for (auto &a : out.referencevec) {
-        report.referencevec.push_back(a);
-    }
+    report.referencevec.reserve(out.referencevec.size());
+    std::copy(out.referencevec.begin(), out.referencevec.end(), std::back_inserter(report.referencevec));
 
-    for (auto &a : out.fresnelvec) {
-        report.fresnelvec.push_back(a);
-    }
+    report.fresnelvec.reserve(out.fresnelvec.size());
+    std::copy(out.fresnelvec.begin(), out.fresnelvec.end(), std::back_inserter(report.fresnelvec));
 
-    for (auto &a : out.fresnel60vec) {
-        report.fresnel60vec.push_back(a);
-    }
+    report.fresnel60vec.reserve(out.fresnel60vec.size());
+    std::copy(out.fresnel60vec.begin(), out.fresnel60vec.end(), std::back_inserter(report.fresnel60vec));
 
-    for (auto &a : out.curvaturevec) {
-        report.curvaturevec.push_back(a);
-    }
+    report.curvaturevec.reserve(out.curvaturevec.size());
+    std::copy(out.curvaturevec.begin(), out.curvaturevec.end(), std::back_inserter(report.curvaturevec));
 
-    for (auto &a : out.profilevec) {
-        report.profilevec.push_back(a);
-    }
+    report.profilevec.reserve(out.profilevec.size());
+    std::copy(out.profilevec.begin(), out.profilevec.end(), std::back_inserter(report.profilevec));
 
-    for (auto &a : out.distancevec) {
-        report.distancevec.push_back(a);
-    }
+    report.distancevec.reserve(out.distancevec.size());
+    std::copy(out.distancevec.begin(), out.distancevec.end(), std::back_inserter(report.distancevec));
 
     report.image_data.reserve(out.imagedata.size());
-    for (auto &pixel : out.imagedata) {
-        report.image_data.push_back(pixel);
-    }
+    std::copy(out.imagedata.begin(), out.imagedata.end(), std::back_inserter(report.image_data));
 
     return report;
 }
